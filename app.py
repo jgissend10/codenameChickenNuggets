2eb3233cd82e27c51721add5cafb15cbb087a7b2
James Gissendaner <jamestgissendanerjr@gmail.com>
2014-11-22 17:35:51 -0500

    Herokuized

--- app.py
+++ app.py
@@ -137,4 +137,5 @@ def contents(owner, repo, path):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
